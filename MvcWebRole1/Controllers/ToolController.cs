82e45a07356eb42973c70c7975dbf7220f952a2f
Sehin <Toribash.ds@gmail.com>
2015-10-06 17:23:04 +0300

    some styles + menu

---
+++ MvcWebRole1/Controllers/ToolController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebRole1.Controllers
+{
+    public class ToolController : Controller
+    {
+        //
+        // GET: /Tool/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SocStudio()
+        {
+            return View();
+        }
+    }
+}
